2bb4d0e1e60b52251d25df0426f67a3508505ee8
José Enrique Aguirre Chavez <jotae_11@hotmail.com>
2021-09-15 19:33:25 -0500

    Router and App vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+     <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      :width="200"
+      permanent
+      expand-on-hover
+      dark
+    >
+      <v-list dense nav>
+        <v-list-item link :to="{path:'/match'}">
+          <v-list-item-icon>
+            <v-icon>mdi-abacus</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title><span>Skills Match</span></v-list-item-title>
+        </v-list-item>
+        <v-list-item link :to="{path:'/roadmap'}">
+          <v-list-item-icon>
+            <v-icon>mdi-flag-checkered</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title><span>Skills Roadmap</span></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="#2A3B4D" dark>
+      <v-toolbar-title class="titulo">
+        Torre Skills Tools
+        <small>v1</small>
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-content class="grey lighten-5">
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+          <!--<loader></loader>-->
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+   <v-footer>
+      <v-spacer></v-spacer>
+      <div>JProjects - Torre Technical Test &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    right: null,
+  }),
+};
+</script>
+<style scoped>
+* {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 300;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.titulo {
+  font-size: 20px;
+}
+.my-input input{
+  text-transform: uppercase
 }
 </style>
